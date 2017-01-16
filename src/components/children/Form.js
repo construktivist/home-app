964198740c82389251f026bf310f4c4b9c2654f1
masonposch <masonposch@gmail.com>
2017-01-16 03:10:17 -0600

    Made function adjustments to intial landing page form

--- src/components/children/Form.js
+++ src/components/children/Form.js
@@ -5,20 +5,60 @@ import React from 'react';
 
 var Form = React.createClass({
 	
-	render: function(){
+	getInitialState: function() {
+
+		return {
+			projectRequest: '',
+			city: '',
+			stateTerritory: ''
+		}
+
+	},
+
+	handleChange: function() {
+
+		return function (e) {
+			var state = {};
+			state[key] = e.target.value;
+			this.setState(state);
+		}.bind(this);
+	
+	},
+
+	render: function() {
+
+		
 
 			return(
+				<div>
 
-				<form onSubmit={this.handleSubmit}>
+				<form>
+
+					<input
+						value={this.state.projectRequest}
+						placeholder="What are you looking for?"
+						onChange={this.handleChange('projectRequest')} />
+						<br />
+
+					<input
+						value={this.state.city}
+						placeholder="City"
+						onChange={this.handleChange('city')} />
+					
 				
-					<input type="text" className="form-control" id="term" placeholder="What kind of service are you looking for?" onChange={this.handleChange} required/><br />
-					<span>
-						<input type="text" className="form-control" id="term" placeholder="City" onChange={this.handleChange} required/>
-						<input type="text" className="form-control" id="term" placeholder="State" onChange={this.handleChange} required/>
-					</span>
-					<button className="btn btn-primary">Add new post</button>
+					<input
+						value={this.state.stateTerritory}
+						placeholder="State"
+						onChange={this.handleChange('stateTerritory')} />
+						<br />
+
+					<button className="btn btn-primary">Find your solution</button>
 
 				</form>
+
+				
+
+				</div>
 
 			)
 
@@ -27,3 +67,11 @@ var Form = React.createClass({
 });
 
 module.exports = Form;
+
+
+// <input type="text" className="form-control" id="term" placeholder="What kind of service are you looking for?" onChange={this.handleChange} required/><br />
+// 					<span>
+// 						<input type="text" className="form-control" id="term" placeholder="City" onChange={this.handleChange} required/>
+// 						<input type="text" className="form-control" id="term" placeholder="State" onChange={this.handleChange} required/>
+// 					</span>
+// 					<button className="btn btn-primary">Add new post</button>

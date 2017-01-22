417a1c16c8007ea6f5c57444e6d47b619b6f78a2
Keegan Kelly <keegankelly1@gmail.com>
2017-01-21 21:44:48 -0600

    Adding morgan for additional logging

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
+var logger = require("morgan");
 const mongoose = require("mongoose");
 const methodOverride = require('method-override');
 const path = require('path');
@@ -12,7 +13,7 @@ mongoose.Promise = Promise;
 
 
 //model controllers
-import Search from "./controllers/Search"
+const Search = require("./controllers/search");
 const clients_controller = require('./controllers/clients_controller');
 
 // instantiatize express
@@ -31,8 +32,11 @@ app.use(methodOverride('_method'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(path.join(__dirname, 'public')));

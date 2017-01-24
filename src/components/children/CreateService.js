0ae53aa48cf440153e21b093a6b4380efcd16a9b
Mason Posch <masonposch@gmail.com>
2017-01-24 01:42:15 -0600

    fixing createService checkboxes

--- src/components/children/CreateService.js
+++ src/components/children/CreateService.js
@@ -12,16 +12,38 @@ class CreateService extends React.Component{
 	constructor(){
 		super();
 
-		this.state = {
-			services: ["Woodwork", "Upolstery", "Landscaping", "Electrical Maintainence", "Metal Work", "Roofing", "Gardening", "Painting", "Hammering", "Moving Services"],
-			service: ""
-		}
+		// this.state = {
+		// 	services: ["Woodwork", "Upolstery", "Landscaping", "Electrical Maintainence", "Metal Work", "Roofing", "Gardening", "Painting", "Hammering", "Moving Services"],
+		// 	service: ""
+		// }
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(event){
+		event.preventDefault();
+		alert('These boxes were checked: ' + {value: event.target.value});
 	}
 
 	render(){
 
 		var contentStyle = {
 			zIndex: '1'
+		};
+
+		var formStyle = {
+			position: "relative",
+			left: "25%",
+			top: "50px"		
+		};
+
+		var buttonStyle = {
+			width: "600px",
+			height: "40px",
+			margin: "0 0 20px 0",
+			borderRadius: "7px",
+			borderStyle: "none",
+			paddingLeft: "10px"
 		};
 
 		return(
@@ -33,11 +55,15 @@ class CreateService extends React.Component{
 
 					<Navbar />
 
-					<form>
-					 	{this.state.services.map((service) => <CreateServiceServices type="checkbox" name={service} value={service} key={service} service={service} />)}
-					</form>
+					<form style={formStyle} onSubmit={this.handleSubmit}>
 
-					<CreateServiceForm />
+					 	<CreateServiceServices />
+
+						<CreateServiceForm />
+
+						<button type="submit" className="btn btn-primary" style={buttonStyle}>Create your service</button>
+
+					</form>
 
 				</div>
 			</div>
--- src/components/children/createServiceChildren/CreateServiceServices.js
+++ src/components/children/createServiceChildren/CreateServiceServices.js
@@ -2,16 +2,68 @@ import React from "react";
 
 class CreateServiceServices extends React.Component {
 
-	render() {
-		return(
-			<div>
+	constructor(){
+		super();
 
-				<input type={this.props.type} value={this.props.service}/>{this.props.service}
+		this.state = {
+			
+			data: [
 
-			</div>
+				{id: 'Woodwork', selected: false},
+				{id: 'Upholstery', selected: false},
+				{id: 'Landscaping', selected: false},
+				{id: 'Electrical Maintainence', selected: false},
+				{id: 'Metal Work', selected: false},
+				{id: 'Roofing', selected: false},
+				{id: 'Gardening', selected: false},
+				{id: 'Painting', selected: false},
+				{id: 'Hammering', selected: false},
+				{id: 'Moving Services', selected: false}
 
-		)
+			]
+		}
+
 	}
+
+	render() {
+		var checks = this.state.data.map(function(d) {
+            return (
+                <div key={d.id}>
+                    <input type="checkbox" data-id={d.id} checked={d.selected} onChange={this.changeSelection.bind(this, d.id)} />
+                    {d.id}
+                    <br />
+                </div>
+            );
+        }.bind(this));
+        return (
+        	<div>
+                <input type="checkbox" ref="globalSelector" onChange={this.changeAllChecks} />
+                <br />
+                {checks}
+            </div>
+        );
+	}
+
+	changeSelection(id){
+		var state = this.state.data.map(function(d){
+			return {
+				id: d.id,
+				selected: (d.id === id ? !d.selected : d.selected)
+			}
+		});
+
+		this.setState({ data: state });
+		console.log()
+	}
+
+	changeAllChecks() {
+        var value = this.refs.globalSelector.getDOMNode().checked;
+        var state = this.state.data.map(function(d) {
+            return { id: d.id, selected: value };
+        });
+
+        this.setState({ data: state });
+    }
 
 }
 

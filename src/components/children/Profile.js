cfcdcfe6acdc2e7e973183d1351a6d7daaa784c7
ShannonGreen27 <shannon.jm.green@gmail.com>
2017-01-26 18:31:40 -0600

    small changes

---
+++ src/components/children/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ProfileForm from './profile/ProfileForm';
+
+
+export default class Profile extends React.Component { 
+	constructor(){
+		super();
+		this.state
+	}
+	render() {
+
+		return(
+
+			<div>
+				<ProfileForm />
+			</div>
+
+		)
+	}
+
+}
+
+module.exports = SignIn;

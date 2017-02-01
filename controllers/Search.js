8ca2e83bb95144b2a503b64cc5f294033d8d8c40
Keegan Kelly <keegankelly1@gmail.com>
2017-01-31 19:27:00 -0600

    Search feature now completely functional

--- controllers/Search.js
+++ controllers/Search.js
@@ -4,19 +4,20 @@ const router = express.Router();
 const User = require("../models/User")
 
 router.get("/", function(req, res){
-  console.log(req.query);
-  //
-  // User
-  //   .find(req.body)
-  //   .exec(function(err, result){
-  //     if (err){
-  //       console.log(err);
-  //     }
-  //     else{
-  //       console.log(result);
-  //       res.send(result);
-  //     }
-  //   });
+  console.log(req.query.service);
+   User
+     .find({
+       serviceOffered: { $in: [req.query.service]} 
+     })
+     .exec(function(err, result){
+       if (err){
+         console.log(err);
+       }
+       else{
+         console.log(result);
+         res.send(result);
+       }
+     });
 });
 
 module.exports = router;

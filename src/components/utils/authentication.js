6dfd0c354bb2bff5a4f30b8de80c708ad784b012
Mason Posch <masonposch@gmail.com>
2017-02-07 21:22:24 -0600

    Resolving Shannon's BIG mistake

--- src/components/utils/authentication.js
+++ src/components/utils/authentication.js
@@ -3,26 +3,25 @@ import axios from "axios";
 module.exports = {
 
 	login(credentials, cb) {
+		// console.log(credentials);
+		cb = arguments[arguments.length - 1]
 		if (localStorage.token) {
-			cb(true)
+			if (cb) cb(true)
 			this.onChange(true)
 			return
 		}		
 		axios.post("/user/login", credentials).then((result)=>{
 			console.log(result)
 			if(result.data.authenticated) {
-				localStorage.token = result.data.session.passport.user
-				cb(true)
+				localStorage.token = result.token
+				if (cb) cb(true)
 				this.onChange(true)
+			} else {
+				if (cb) cb(false)
+				this.onChange(false)				
 			}
-			//  else {
-			// 	if (cb) cb(false)
-			// 	this.onChange(false)				
-			// }
 		}).catch((err)=>{
-			console.log(err.statusCode() ===401)
-			cb(false)
-			this.onChange(false)
+			// console.log(err)
 		})
 	},
 
@@ -36,16 +35,15 @@ module.exports = {
 	},
 
 	logout() {
+		// console.log(credentials);
 	    const token = localStorage.getItem('token')
     	localStorage.removeItem('token')
-		axios.get("/user/logout").then((result)=>{
-			console.log(result)
-		})
+		axios.get("/user/logout")
     	this.onChange(false)
 	},
 
 	loggedIn() {
-		return !!this.getToken()
+		return !!localStorage.token
 	},
 
  	onChange() {}	

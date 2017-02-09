3a2ddc2d1aa06b3075ceaba4ddb11c3519a9a6cf
Keegan Kelly <keegankelly1@gmail.com>
2017-02-08 23:10:44 -0600

    Added button to SearchResults compoment and updated rendering

--- src/components/children/searchPageChildren/SearchResults.js
+++ src/components/children/searchPageChildren/SearchResults.js
@@ -1,22 +1,73 @@
 import React from "react";
 
 export default class SearchResults extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			profile: ""
+		}
+		this.handleClick = this.handleClick.bind(this);
+	};
+
+	handleClick(id){
+		console.log(id);
+
+	// 	helpers.getProfile(this.state.profile)
+	// 		.then(function(response){
+	//
+	//
+	// 		}.bind(this))
+	//
+ 	};
 
 	render() {
 		return(
 			<div>
-					<div className="panel panel-default">
-						<div className="panel-heading">
-							{this.props.results.name}
+				<div className="row">
+					<div className="col-xs-4">
+						<div className="panel panel-primary">
+							<div className="panel-heading">
+									<h2 className="panel-heading">
+										{this.props.results.name}
+									</h2>
+							</div>
+							<div className="panel-body">
+								<div className="row">
+
+									<div className="col-xs-4">
+											<img
+												src="https://inomics.com/sites/default/files/pictures/picture-95970-1460131169.png"
+											alt="profile image" />
+									</div>
+
+									<div className="col-xs-8">
+										<div className="row">
+											<div className="col-xs-12">
+												Location: {this.props.results.city}, {this.props.results.state}
+											</div>
+										</div>
+										<div className="row">
+											<div className="col-xs-12">
+													Services: {this.props.results.serviceOffered}
+											</div>
+										</div>
+										<div className="row">
+											<div className="col-xs-12">
+												Avg. Customer Rating: {this.props.results.clientSatisfaction}
+											</div>
+										</div>
+										<div className="row">
+											<div className="col-xs-12">
+												<button onClick={this.handleClick(this.props.results._id)}>View My Profile</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
-						<div className="panel-body">
-							{this.props.results.username}
-							{this.props.results.serviceOffered}
-							{this.props.results.phoneNumber}
-						</div>
+					</div>
 				</div>
 			</div>
-
 		)
 	}
 
